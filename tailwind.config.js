dbe7faf80aeff8a0e8377407b88a69d5c7ed1a7c
Enzo Perego <enzorp@al.insper.edu.br>
2025-09-12 16:11:20 -0300

    Header e Footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'kaiserhaus': {
+          'brown': '#792810',
+          'dark-brown': '#5C1F0C',
+          'light-brown': '#9B3A1A',
+        }
+      },
+    },
   },
   plugins: [],
 }
